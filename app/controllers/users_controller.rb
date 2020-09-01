eb6fc29257a327d005b1efb69599936b6cf437c1
saintlammy <saintlammy@gmail.com>
2020-09-01 19:34:23 +0100

    Created users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
   def new
     @user = User.new
   end
@@ -12,7 +17,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      flash[:notice] = "Your account information was successfully updated"
+      flash[:notice] = 'Your account information was successfully updated'
       redirect_to articles_path
     else
       render 'edit'
@@ -30,7 +35,8 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
-    params.require(:user).permit(:username, :email, :password) 
+    params.require(:user).permit(:username, :email, :password)
   end
 end
